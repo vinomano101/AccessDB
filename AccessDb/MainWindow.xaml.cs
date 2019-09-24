44b10081cfbcbc3697bdc3864ac7c4b382b91fa9
Vincent Manoli <vinomano@ut.utm.edu>
2019-09-24 12:16:03 -0500

    assets button works and so does employee button

--- AccessDb/MainWindow.xaml.cs
+++ AccessDb/MainWindow.xaml.cs
@@ -25,12 +25,77 @@ namespace AccessDb
         public OleDbConnection cn;
         public MainWindow()
         {
-            
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\vinomano\\Desktop\\Database11.accdb";
             InitializeComponent();
-            cn = new OleDbConnection(<  >);
+            cn = new OleDbConnection( connectionString );
         }
 
         private void See_Assets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+            cn.Open();
+
+            OleDbDataReader read = cmd.ExecuteReader();
+
+            string data = "";
+
+            while (read.Read())
+            {
+                data += read[2].ToString() + "\n";
+
+                TextArea.Text = data;
+            }
+
+            cn.Close();
+        }
+
+        private void TextArea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void See_Employees_Click(object sender, RoutedEventArgs e)
+        {
+
+            string query = "select * from Employees";
+
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+            cn.Open();
+
+            OleDbDataReader read = cmd.ExecuteReader();
+
+            string data = "";
+
+            while (read.Read())
+            {
+                data += read[1].ToString() + "\n";
+
+                Employees.Text = data;
+            }
+
+            cn.Close();
+        }
+
+        private void Employees_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void FirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LastName_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
